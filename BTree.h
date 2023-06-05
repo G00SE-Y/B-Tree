e5d047f3df80a67afa454eb4bd42eab6cd1a895a
Ethan <bulldawg0065@gmail.com>
2023-06-05 19:03:40 -0400

    Added most of the code except delete (untested)

--- BTree.h
+++ BTree.h
@@ -1,25 +1,33 @@
+#include <vector>
 
 typedef struct Node {
     
-    int key; // data
-    int nKeys; //number of children
-    struct Node* keys; // pointers to children
-    bool isLeaf;
+    bool isLeaf; // is this node a leaf or not
+    int nKeys; // number of keys in node
+    std::vector<int> keys;
+    std::vector<struct Node*> children; // pointers to children nodes
+    
 }node;
 
 class BTree {
 
     private:
 
-        node* initNode(int v);
+        int size; // branch factor
+
+        node* root;
+        node* initNode();
+        void cleanup(node* r);
+        void splitChild(node* x, int i, node* y);
+        void insertNonfull(node* x, int k);
 
 
     public:
 
         BTree(int branchFactor);
         ~BTree();
-        void insert(int v);
-        bool search(int v);
+        void insert(int k);
+        bool search(node* x, int k);
         
 
 };
